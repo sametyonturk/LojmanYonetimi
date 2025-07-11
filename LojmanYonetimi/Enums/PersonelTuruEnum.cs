9b76156e0a4f1b36e8ba4c6b245d1850836deb91
ramaz <smtyonturk@gmail.com>
2025-07-11 11:08:01 +0300

    PersonelturuEnum eklendi

---
+++ LojmanYonetimi/Enums/PersonelTuruEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LojmanYonetimi.Enums
+{
+    public enum PersonelTuruEnum
+    {
+        [Display(Name = "Akademik")]
+        Akademik = 0,
+
+        [Display(Name = "İdari")]
+        Idari = 1,
+
+        [Display(Name = "Sözleşmeli")]
+        Sozlesmeli = 2
+    }
+}
